1788652fabdfd6ceeec34833ffd0aa3297cc268f
acer <volodymyr4509@gmail.com>
2014-11-24 18:22:58 +0200

    add testrunner to test hibernate working. populate Player entity

---
+++ src/main/java/com/game/mazeapp/dao/UserDaoImpl.java
@@ -0,0 +1,9 @@
+package com.game.mazeapp.dao;
+
+/**
+ * Created by Acer on 11/24/2014.
+ */
+public class UserDaoImpl {
+
+
+}
---
+++ src/main/java/com/game/mazeapp/utils/TestRunner.java
@@ -0,0 +1,31 @@
+package com.game.mazeapp.utils;
+
+import com.game.mazeapp.entity.Player;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ * Created by Acer on 11/24/2014.
+ */
+public class TestRunner {
+    public static void main(String[]args){
+        HibernateHelper hibernateHelper = new HibernateHelper();
+        Player player1 = new Player();
+        player1.setEmail("slsadf");
+        player1.setFirstName("firstnAME");
+        player1.setHealth(100);
+        player1.setLastName("sdafasfasfasf");
+        player1.setNickName("nickname");
+        Session session =  hibernateHelper.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            session.save(player1);
+            transaction.commit();
+        }catch (Exception e){
+            transaction.rollback();
+        }
+        hibernateHelper.shutdown();
+
+    }
+}
