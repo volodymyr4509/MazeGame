6dcf52668b3159c36621e13a488cebc136b991a0
volodymyr4509 <volodymyr4509@gmail.com>
2015-01-31 22:21:36 +0200

    add profile.js

--- src/main/webapp/resources/files/javascripts/profile.js
+++ src/main/webapp/resources/files/javascripts/profile.js
@@ -1,32 +1,13 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Home
-  Date: 06.12.2014
-  Time: 16:38
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Profile</title>
-</head>
-<body>
-<div id="userName" align="center">
-    ${pageContext.request.userPrincipal.name} <br>
-</div>
-
-    <img src="/resources/avapes.jpg" />
-
-<hr>
-<!-- Player info-->
-<button type="button" onclick="loadProfileInfo()">show profileinfo</button>
-<script>
+/**
+ * Created by Home on 31.01.2015.
+ */
+$( document ).ready(function() {
     function loadProfileInfo(){
         $.ajax({
             type:"GET",
             url: "/profile",
             dataType: 'json',
-            data: $("#userName"),
+            data: myJson,
             contentType: 'application/json; charset=utf-8',
             async: true,    //Cross-domain requests and dataType: "jsonp" requests do not support synchronous operation
             cache: false,    //This will force requested pages not to be cached by the browser
@@ -39,7 +20,6 @@
             }
         });
     }
-</script>
 
-</body>
-</html>
+
+});
